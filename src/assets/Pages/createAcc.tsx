0e680c60c319f0b15839fb2afe7213f7bf39ebb9
Jack Hesslein <jackhesslein@gmail.com>
2024-12-16 21:06:01 -0500

    made first additions to home page

--- src/assets/Pages/createAcc.tsx
+++ src/assets/Pages/createAcc.tsx
@@ -1,14 +1,18 @@
 
+export default function CreateAccount() {
 
+return (
 <div>
-    <h1 class="formTitle">Create an Account</h1>
-    <form class="form">
+    <h1 className="formTitle">Create an Account</h1>
+    <form className="form">
         <div>
-            <label class="name">Name:</label>
-            <input class="input" type="text" placeholder="Name" />
+            <label className="name">Name:</label>
+            <input className="input" type="text" placeholder="Name" />
         </div>
         <div>
-            <button class="submit" type="submit">Login</button>
+            <button className="submit" type="submit">Login</button>
         </div>
     </form>
 </div>
+);
+}
