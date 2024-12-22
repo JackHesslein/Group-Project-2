0b23aa74b5f75caaf2f9ea908a3c44540f287b14
Pink727 <doc72789@gmail.com>
2024-12-21 23:23:19 -0800

    Comment out axios import in Home component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+// import axios from 'axios';
 import WeatherCard from '../components/WeatherCard';
 import NewsCard from '../components/NewsCard';
 import '../styles/Home.css';
